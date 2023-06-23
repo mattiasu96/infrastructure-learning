45c766ed7b9b367914a17bb01e89ff2a0c346cfc
mattiasu96 <mattia.surricchio@prima.it>
2023-06-23 16:07:11 +0200

    Added t2 machine to infra building

--- main.tf
+++ main.tf
@@ -6,10 +6,25 @@ terraform {
       name = "learn-terraform-github-actions"
     }
   }
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
 }
 
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
   }
 }
